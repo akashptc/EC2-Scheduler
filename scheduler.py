094ebe8d61ac660fbe84ff8414fe62fbdc3ffe3a
slashr <slashr@users.noreply.github.com>
2016-10-20 19:14:06 +0530

    Fixed error in fetching instance IDs

--- scheduler.py
+++ scheduler.py
@@ -3,13 +3,20 @@ import boto3
 ec2 = boto3.client('ec2')
 instance_ids = []
 filters = [{'Name':'tag:Autostop', 'Values':['Yes']}]
+i=0
 
 #Filter instances by tags
+print "Getting list of Autostop instances..."
 instances_list = ec2.describe_instances(Filters=filters)
 
+
+print len(instances_list['Reservations'])," instances will be stopped"
+
+
 #Get instance IDs of filtered instances
-for instances in instances_list:
-	instance_ids.append(instances_list['Reservations'][0]['Instances'][0]['InstanceId'])
+for instance in instances_list['Reservations']:
+	instance_ids.append(instance['Instances'][0]['InstanceId'])
 
 #Stop instances
+print "Stopping instances"
 ec2.stop_instances(InstanceIds=instance_ids)
